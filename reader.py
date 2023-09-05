1360bb44f37b5e2b47d34b7483a304f83eedba78
Albert Caballero <albert@Alberts-MBP.izzigo.tv>
2023-09-04 21:03:48 -0600

    pretty much done

--- reader.py
+++ reader.py
@@ -1,26 +1,40 @@
 import tkinter as tk
+import time
 
-def reader(list):
-	i = 0
-	print (len(list))
-	while (i < len(list)):
-		word.config(text=list[i])
-		print (list[i])
-		i += 1
+i = 0
+speed = 1
 
-def start():
-	text = entry.get()
-	print (text)
+def reset():
+	global i
+	i = 0;
+	word.config(text="")
+
+def get_speed(val):
+	global speed
+	speed = int(val)/1.73
+
+def reader():
+	global i, speed
+	print (int(300*(1/speed)))
+	text = entry.get(1.0, 'end')
 	list = text.split()
-	reader(list)
+	if (i < len(list)):
+		word.config(text=list[i])
+		i += 1
+		window.after(int(300*(1/speed)), reader)
+	return
+	# if (i == len(list)):
+	#  	i = 0
 
 window = tk.Tk()
 window.geometry("400x400")
 window.title("A faster Reader")
-window.configure(bg='green')
-entry = tk.Entry(window, relief="groove", background="blue")
+entry = tk.Text(window, relief="groove", height=5, wrap="word")
 entry.pack(pady=10)
-tk.Button(window, text="go", command=start).pack(pady=10)
+tk.Button(window, text="go", command=reader).pack(pady=10)
+tk.Button(window, text="reset", command=reset).pack(pady=2)
+speed_scale = tk.Scale(window, orient="horizontal", command=get_speed, from_=1, to=5)
+speed_scale.pack(pady=10)
 word = tk.Label(window, text="")
 word.pack(pady=10)
 
