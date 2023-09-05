f7da3f1b26c818bf197848e3f98414f53a2c66a2
Albert Caballero <albert@Alberts-MBP.lan>
2023-09-04 18:16:01 -0600

    fjkasldjk

--- reader.py
+++ reader.py
@@ -1,8 +1,18 @@
 import tkinter as tk
 
+def reader(list):
+	i = 0
+	print (len(list))
+	while (i < len(list)):
+		word.config(text=list[i])
+		print (list[i])
+		i += 1
+
 def start():
 	text = entry.get()
 	print (text)
+	list = text.split()
+	reader(list)
 
 window = tk.Tk()
 window.geometry("400x400")
@@ -11,5 +21,7 @@ window.configure(bg='green')
 entry = tk.Entry(window, relief="groove", background="blue")
 entry.pack(pady=10)
 tk.Button(window, text="go", command=start).pack(pady=10)
+word = tk.Label(window, text="")
+word.pack(pady=10)
 
 window.mainloop()
