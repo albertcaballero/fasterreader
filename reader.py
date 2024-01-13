cc298a0c0079188955b39a151f9fe345f066596a
Albert Caballero Coll <alcaball@car11s4.42barcelona.com>
2024-01-13 16:35:11 +0100

    added pause button, works iffy

--- reader.py
+++ reader.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 i = 0
 interval = 300
+pause = 0;
 
 def reset():
 	global i
@@ -14,15 +15,22 @@ def wpm_to_ms():
 	interval = int(1/(wpm/60)*1000)
 
 def reader():
-	global i, interval
+	global i, interval, pause
 	wpm_to_ms()
 	text = entry.get(1.0, 'end')
 	list = text.split()
-	if (i < len(list)):
+	if (i < len(list) and pause == 0):
 		word.config(text=list[i])
 		i += 1
 		window.after(interval, reader)
 	return
+
+def pause_btn():
+	global pause
+	if (pause == 0):
+		pause = 1
+	elif (pause == 1):
+		pause = 0;
 
 window = tk.Tk()
 window.geometry("400x400")
@@ -33,6 +41,7 @@ wpm_box = tk.Entry(window, width=6)
 wpm_box.pack(pady=5)
 tk.Button(window, text="go", command=reader).pack(pady=10)
 tk.Button(window, text="reset", command=reset).pack(pady=2)
+tk.Button(window, text="pause/play", command=pause_btn).pack(pady=2)
 word = tk.Label(window, text="", font="arial 22")
 word.pack(pady=20)
 
